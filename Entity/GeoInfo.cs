f7cb02641dd85251db527821ea559c2a6e1b7765
张选志 <964849125@qq.com>
2024-03-06 10:54:23 +0800

    初始化代码

---
+++ Entity/GeoInfo.cs
@@ -0,0 +1,84 @@
+﻿using FreeSql.DataAnnotations;
+using Microsoft.SqlServer.Types;
+using System;
+using System.Collections.Generic;
+
+namespace GeoTool.Entity
+{
+    [Table(Name = "geo_info")]
+    public class GeoInfo
+    {
+
+        public int Id { get; set; }
+
+        public int Pid { get; set; }
+
+        public int Dep { get; set; }
+
+        public string Name { get; set; }
+
+        public string ExtPath { get; set; }
+
+        [Column(
+             DbType = "geometry",
+             RewriteSql = "geometry::STGeomFromText({0},0).MakeValid()",
+             RereadSql = "{0}.STAsText()"
+             )]
+        public string Geo { get; set; }
+
+        [Column(
+            DbType = "geometry",
+            RewriteSql = "geometry::STGeomFromText({0},0).MakeValid()",
+            RereadSql = "{0}.STAsText()"
+            )]
+        public string Polygon { get; set; }
+
+
+        public GeoInfo(string data)
+        {
+            if (!string.IsNullOrEmpty(data)) {
+                var dataArr = data.Split(",");
+                Id = Convert.ToInt32(dataArr[0]);
+                Pid = Convert.ToInt32(dataArr[1]);
+                Dep = Convert.ToInt32(dataArr[2]);
+                Name = dataArr[3].Trim().TrimStart('"').TrimEnd('"');
+                ExtPath = dataArr[4].Trim().TrimStart('"').TrimEnd('"');
+                var geo = dataArr[5].Trim().TrimStart('"').TrimEnd('"');
+
+                if (geo != "EMPTY")
+                {
+                    Geo = $"POINT ({geo})";
+                }
+
+                var polygon = data.Replace(String.Join(',', dataArr.Take<string>(6)) + ",", "").Trim().TrimStart('"').TrimEnd('"');
+                if (polygon != "EMPTY")
+                {
+                    var x = polygon.Split(";");
+                    if (x.Count() > 1)
+                    {
+                        var list = new List<string>();
+                        foreach (var item in x)
+                        {
+                            var item2 = item.Split(',').ToList();
+                            item2.Add(item2[0]);
+                            list.Add($"(({string.Join(',', item2)}))");
+                        }
+                      
+                        Polygon = $"MULTIPOLYGON ({string.Join(',', list)})";
+                    }
+                    else
+                    {
+                        var list = polygon.Split(',').ToList();
+                        list.Add(list[0]);
+                        Polygon = $"POLYGON (({string.Join(',', list)}))";
+                    }
+
+                }
+            }
+
+          
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GeoTool.Entity;
+string runDirectory = AppDomain.CurrentDomain.BaseDirectory;
+string filePath = Path.Combine(runDirectory, "ok_geo.csv");
+Console.WriteLine("请输入数据库链接字符串：");
+var com=Console.ReadLine();
+var connectionString = "";
+if (com != null) {
+    connectionString = com;
+}
+if (!File.Exists(filePath)) {
+    Console.WriteLine("未找到文件:"+ filePath);
+    Console.ReadKey();
+    return;
+}
+Console.WriteLine("开始处理数据...");
+
+IFreeSql fsql = new FreeSql.FreeSqlBuilder()
+    .UseConnectionString(FreeSql.DataType.SqlServer, connectionString)
+    .UseAutoSyncStructure(true)
+    .UseMonitorCommand(cmd => { })
+    .Build();
+using (StreamReader reader = new StreamReader(filePath))
+{
+    var i = 0;
+    while (!reader.EndOfStream)
+    {
+        string line = reader.ReadLine();
+        if (i!= 0) {
+            var entity = new GeoTool.Entity.GeoInfo(line);
+            Console.WriteLine(entity.Name+" "+entity.ExtPath);
+            fsql.InsertOrUpdate<GeoInfo>()
+  .SetSource(entity) 
+  .ExecuteAffrows();
+        }
+       
+        i++;
+    }
+    Console.WriteLine("共处理数据："+(i-1)+"条");
+    Console.ReadKey();
+}
+
